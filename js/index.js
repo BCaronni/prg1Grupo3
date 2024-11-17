038e40952ea492a2b359ba9e352a1d9527bffb9d
BCaronni <bcaronni@udesa.edu.ar>
2024-11-17 10:26:37 -0300

    Avance con index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,26 @@
+//Hacemos solicitud GET de la api de recetas (Tenemos que ver si hay que usar la de search o la de all recipes!!!)
 fetch('https://dummyjson.com/recipes')
-.then(res => res.json())
-.then(console.log);
+    //esto veamos si hay que dejarlo porque venia con la pagina pero no se si no hay que hacerlo como el de rick y morty.
+    .then(res => res.json())
+    //.then(console.log);
+    
+    //Comento para despues corregir. Genero una variable para la lista de recetas
+    .then(function (data){
+        let recetas = data.results;
+        //Empiezo una cadena vacia para el html
+        let cadena = "";
+        let recipeList = document.querySelector(".recipeList");
+
+        //estilos?
+
+        //Recorro el array
+        for (let i = 0; i < recetas.length; i++){
+            cadena +=  `
+
+            `
+        }
+
+
+    })
 
 
