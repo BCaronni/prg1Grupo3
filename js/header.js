9ef57f2f4b22944fd34054b0d04c3873f1d243f5
beniciolevy <blevy@udesa.edu.ar>
2024-11-17 14:29:03 -0300

    Configure la API, termine form de header y termine estructura de index.html

--- js/header.js
+++ js/header.js
@@ -1,4 +1,14 @@
-function chequearBusqueda(){
-    let searchInput = document.querySelector("#searchInput")
-    let errorMessage = document.querySelector("#error-message")
-}
+let buscarForm = document.querySelector("#buscarForm");
+
+buscarForm.addEventListener("submit", function(event) {
+    let searchInput = document.querySelector("#searchInput");
+    let errorMessage = document.querySelector("#mensaje-error");
+
+    // Validación de la longitud del texto
+    if (searchInput.value.length < 3) { // Corregido el paréntesis aquí
+        errorMessage.style.display = "block"; // Muestra el mensaje de error
+        event.preventDefault(); // Detiene el envío del formulario
+    } else {
+        errorMessage.style.display = "none"; // Oculta el mensaje de error
+    }
+});
--- js/index.js
+++ js/index.js
@@ -1,26 +1,49 @@
 //Hacemos solicitud GET de la api de recetas (Tenemos que ver si hay que usar la de search o la de all recipes!!!)
 fetch('https://dummyjson.com/recipes')
     //esto veamos si hay que dejarlo porque venia con la pagina pero no se si no hay que hacerlo como el de rick y morty.
-    .then(res => res.json())
+    .then(function (response){
+        return response.json();
+    })
     //.then(console.log);
     
     //Comento para despues corregir. Genero una variable para la lista de recetas
     .then(function (data){
-        let recetas = data.results;
+        let recetas = data.recipes;
         //Empiezo una cadena vacia para el html
         let cadena = "";
         let recipeList = document.querySelector(".recipeList");
 
         //estilos?
+        recipeList.style.display = "flex";
+        recipeList.style.display = "wrap";
+        recipeList.style.display = "space-evenly";
+
+
 
         //Recorro el array
         for (let i = 0; i < recetas.length; i++){
-            cadena +=  `
+            cadena += 
+             `<article>
+                     <img src="${recetas[i].image}" alt="">
+                     <h3>${recetas[i].name}</h3>
+                      <p><strong>Dificultad:</strong> ${recetas[i].difficulty}</p>
+                      <p><a href="receta.html?id=${recetas[i].id}" style="text-decoration: none; color: blue;">Ver más</a></p>
+                     
+               </article>
 
-            `
+            `;
         }
 
 
-    })
+    
+    recipeList.innerHTML = cadena;
+})
+    .catch(function (error) {
+        // En caso de error en la solicitud, mostramos el mensaje de error en la consola
+        console.log("Error al cargar las recetas: ", error);
+      });
+      
+
+    
 
 
