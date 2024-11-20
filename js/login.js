a8eff5a8f4454eb6fb1e23fd5425e48f21657243
beniciolevy <blevy@udesa.edu.ar>
2024-11-20 11:14:18 -0300

    cree el form de login.html y js

---
+++ js/login.js
@@ -0,0 +1,20 @@
+let loginForm = document.querySelector("#loginForm")
+
+loginForm.addEventListener("submit", function(event){
+    let email = document.querySelector("#email")
+    let password = document.querySelector("#password")
+
+
+    if (email.value == ""){
+        event.preventDefault();
+        alert("El email no puede estar vacio");
+        
+
+    }
+
+    else if (password.value == ""){
+        event.preventDefault();
+        alert("La contraseña no puede estar vacia")
+    }
+
+});
